5da9342fe19ad555fd34ff27233fe483e2d94d23
Vasanth <vasanthaj739@gmail.com>
2025-07-21 17:06:48 +0530

    initial commit

---
+++ src/main/java/com/example/face_recognition_applicaton/FaceRecognitionApplicatonApplication.java
@@ -0,0 +1,13 @@
+package com.example.face_recognition_applicaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceRecognitionApplicatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceRecognitionApplicatonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/face_recognition_applicaton/FaceRecognitionApplicatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.face_recognition_applicaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceRecognitionApplicatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
